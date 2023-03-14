75a2a81f10a36d537afade7953061dc697b52cb8
zippelgazz <andrisuvorov@gmail.com>
2023-03-14 08:01:17 +0200

    commit

--- src/js/sprite.js
+++ src/js/sprite.js
@@ -14,29 +14,40 @@ const spriter = new SVGSpriter({
     }
 });
 
-spriter.add(`${__dirname}/../assets/svg/mobile-screen-button-solid.svg`, null, 
-fs.readFileSync(`${__dirname}/../assets/svg/mobile-screen-button-solid.svg`, 'utf-8'));
+const addToSpriter = (path) => {
+    spriter.add(path, null, fs.readFileSync(path, 'utf-8'));
+}
 
-spriter.add(`${__dirname}/../assets/svg/earth-europe-solid.svg`, null, 
-fs.readFileSync(`${__dirname}/../assets/svg/earth-europe-solid.svg`, 'utf-8'));
-
-spriter.add(`${__dirname}/../assets/svg/envelope-solid.svg`, null, 
-fs.readFileSync(`${__dirname}/../assets/svg/envelope-solid.svg`, 'utf-8'));
-
-spriter.add(`${__dirname}/../assets/svg/map-pin-solid.svg`, null, 
-fs.readFileSync(`${__dirname}/../assets/svg/map-pin-solid.svg`, 'utf-8'));
-
-spriter.add(`${__dirname}/../assets/svg/facebook-f.svg`, null, 
-fs.readFileSync(`${__dirname}/../assets/svg/facebook-f.svg`, 'utf-8'));
-
-spriter.add(`${__dirname}/../assets/svg/linkedin-in.svg`, null, 
-fs.readFileSync(`${__dirname}/../assets/svg/linkedin-in.svg`, 'utf-8'));
-
-spriter.add(`${__dirname}/../assets/svg/graduation-cap-solid.svg`, null, 
-fs.readFileSync(`${__dirname}/../assets/svg/graduation-cap-solid.svg`, 'utf-8'));
-
-spriter.add(`${__dirname}/../assets/svg/briefcase-solid.svg`, null, 
-fs.readFileSync(`${__dirname}/../assets/svg/briefcase-solid.svg`, 'utf-8'));
+addToSpriter(`${__dirname}/../assets/svg/mobile-screen-button-solid.svg`);
+addToSpriter(`${__dirname}/../assets/svg/earth-europe-solid.svg`);
+addToSpriter(`${__dirname}/../assets/svg/envelope-solid.svg`);
+addToSpriter(`${__dirname}/../assets/svg/map-pin-solid.svg`);
+addToSpriter(`${__dirname}/../assets/svg/facebook-f.svg`);
+addToSpriter(`${__dirname}/../assets/svg/linkedin-in.svg`);
+addToSpriter(`${__dirname}/../assets/svg/graduation-cap-solid.svg`);
+addToSpriter(`${__dirname}/../assets/svg/briefcase-solid.svg`);
+addToSpriter(`${__dirname}/../assets/svg/book-solid.svg`);
+addToSpriter(`${__dirname}/../assets/svg/camera-solid.svg`);
+addToSpriter(`${__dirname}/../assets/svg/code-solid.svg`);
+addToSpriter(`${__dirname}/../assets/svg/dumbbell-solid.svg`);
+addToSpriter(`${__dirname}/../assets/svg/gamepad-solid.svg`);
+addToSpriter(`${__dirname}/../assets/svg/headphones-solid.svg`);
+addToSpriter(`${__dirname}/../assets/svg/person-walking-solid.svg`);
+addToSpriter(`${__dirname}/../assets/svg/plane-solid.svg`);
+addToSpriter(`${__dirname}/../assets/svg/tree-solid.svg`);
+addToSpriter(`${__dirname}/../assets/svg/html5.svg`);
+addToSpriter(`${__dirname}/../assets/svg/css3.svg`);
+addToSpriter(`${__dirname}/../assets/svg/sass.svg`);
+addToSpriter(`${__dirname}/../assets/svg/bootstrap.svg`);
+addToSpriter(`${__dirname}/../assets/svg/zurb.svg`);
+addToSpriter(`${__dirname}/../assets/svg/js.svg`);
+addToSpriter(`${__dirname}/../assets/svg/jquery.svg`);
+addToSpriter(`${__dirname}/../assets/svg/react.svg`);
+addToSpriter(`${__dirname}/../assets/svg/node.svg`);
+addToSpriter(`${__dirname}/../assets/svg/webpack.svg`);
+addToSpriter(`${__dirname}/../assets/svg/browserify.svg`);
+addToSpriter(`${__dirname}/../assets/svg/npm.svg`);
+addToSpriter(`${__dirname}/../assets/svg/yarn.svg`);
 
 spriter.compile((error, result) => {
     for (const mode of Object.values(result)) {
