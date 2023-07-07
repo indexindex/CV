c221d7a428dc172ce4a4bc4e5931f0746fcfdca6
Andri Suvorov <andrisuvorov@gmail.com>
2023-07-07 12:54:53 +0300

    ADDED - logic for full color theme change
    now when changing colors, both hex and rgb colors change. that makes labels defined with rgb background change color accordingly.

--- src/js/modules/preferences.js
+++ src/js/modules/preferences.js
@@ -37,52 +37,72 @@ const preferences = {
         const { colorInput1, colorInput2, complimentaryInputs, colorThemeReset } = this;
         const rootStyles = getComputedStyle(document.documentElement);
         const primaryColor = rootStyles.getPropertyValue('--color-primary');
+        const primaryColorRGB = rootStyles.getPropertyValue('--color-primary-rgb');
         const secondaryColor = rootStyles.getPropertyValue('--color-secondary');
+        const secondaryColorRGB = rootStyles.getPropertyValue('--color-secondary-rgb');
 
         const changeColor = (variable, color) => {
             document.documentElement.style.setProperty(variable, color);
         }
 
+        const hexToRgb = (hex) => {
+            hex = hex.replace('#', '');
+
+            const r = parseInt(hex.substring(0, 2), 16);
+            const g = parseInt(hex.substring(2, 4), 16);
+            const b = parseInt(hex.substring(4, 6), 16);
+
+            return `${r}, ${g}, ${b}`;
+        }
+
+        const inputChange = (variable, color, expire) => {
+            changeColor(`--${variable}`, color);
+            changeColor(`--${variable}-rgb`, hexToRgb(color));
+            cookie.setCookie(`${variable}-set`, color, expire);
+            cookie.setCookie(`${variable}-rgb-set`, hexToRgb(color), expire);
+        }
+
+
+
         changeColor('--color-primary', cookie.getCookie('color-primary-set') || primaryColor);
+        changeColor('--color-primary-rgb', cookie.getCookie('color-primary-rgb-set') || primaryColorRGB);
         changeColor('--color-secondary', cookie.getCookie('color-secondary-set') || secondaryColor);
+        changeColor('--color-secondary-rgb', cookie.getCookie('color-secondary-rgb-set') || secondaryColorRGB);
 
         colorInput1.value = cookie.getCookie('color-primary-set') || primaryColor.trim();
         colorInput2.value = cookie.getCookie('color-secondary-set') || secondaryColor.trim();
 
-        colorInput1.addEventListener('input', ({ target }) => {
-            changeColor('--color-primary', target.value);
-            cookie.setCookie('color-primary-set', target.value, 1);
-        })
-        colorInput2.addEventListener('input', ({ target }) => {
-            changeColor('--color-secondary', target.value);
-            cookie.setCookie('color-secondary-set', target.value, 1);
-        })
+        colorInput1.addEventListener('input', ({ target }) => { inputChange('color-primary', target.value, 1); })
+        colorInput2.addEventListener('input', ({ target }) => { inputChange('color-secondary', target.value, 1); })
 
         complimentaryInputs.forEach(input => {
             switch(input.getAttribute('id')) {
                 case 'red':
                     input.style.backgroundColor = '#c62d2d';
+                    input.setAttribute('data-color-hex', '#c62d2d');
                     break;
                 case 'green':
                     input.style.backgroundColor = '#2dc653';
+                    input.setAttribute('data-color-hex', '#2dc653');
                     break;
                 case 'purple':
                     input.style.backgroundColor = '#a02dc6';
+                    input.setAttribute('data-color-hex', '#a02dc6');
                     break;
                 case 'yellow':
                     input.style.backgroundColor = '#c6aa2d';
+                    input.setAttribute('data-color-hex', '#c6aa2d');
                     break;
             }
 
-            input.addEventListener('click', ({ target }) => {
-                changeColor('--color-secondary', target.style.backgroundColor);
-                cookie.setCookie('color-secondary-set', target.style.backgroundColor, 1);
-            })
+            input.addEventListener('click', ({ target }) => { inputChange('color-secondary', target.getAttribute('data-color-hex'), 1); })
         })
 
         colorThemeReset.addEventListener('click', () => {
-            changeColor('--color-secondary', secondaryColor);
-            cookie.setCookie('color-secondary-set', secondaryColor, 0);
+            colorInput1.value = primaryColor;
+            colorInput2.value = secondaryColor;
+            inputChange('color-primary', primaryColor, 0);
+            inputChange('color-secondary', secondaryColor, 0);
         })
     },
     reduceAnimations() {
